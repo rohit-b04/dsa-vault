192dee18386d1a376f64163d141ca7d36c269dbb
Rohit <139196975+rohit-b04@users.noreply.github.com>
2025-04-14 19:19:36 +0530

    Optimized (Tortoise and hare) Approach for middle of LL

--- Linked List/Middle-Of-Linked-List.cpp
+++ Linked List/Middle-Of-Linked-List.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int getSize(ListNode* head) {
-        ListNode* temp = head;
-        int count = 0;
-        while(temp != NULL) {
-            count++;
-            temp = temp -> next;
-        }
-        return count;
-    }
-
+   // Fast and slow approach (Optimized)
     ListNode* middleNode(ListNode* head) {
-        int size = getSize(head);
-        int mid = size/2;
-        if(size % 2 == 0) mid = mid+1;
-        ListNode* temp = head;
-        ListNode* cur;
-        while(mid--) {
-            cur = temp;
-            temp = temp -> next;
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        if(head == nullptr || head -> next == nullptr) return head;
+        if((head->next)->next == nullptr) return head->next;
+
+        while(fast != nullptr) {
+            fast = fast -> next;
+            if(fast == nullptr) break;
+            fast = fast -> next;
+            slow = slow -> next;
+            
         }
-        if(size % 2 == 1) return temp;
-        return cur;
+        return slow;
     }
 };
