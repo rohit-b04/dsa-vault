6e54f68de8bb91537bed801dbf80d8a70cd4d277
Rohit <139196975+rohit-b04@users.noreply.github.com>
2025-10-06 15:33:45 +0530

    Time: 91 ms (84.21%), Space: 13.9 MB (59.43%) - LeetArchive

---
+++ LeetCode/0312-burst-balloons/0312-burst-balloons.cpp
@@ -0,0 +1,25 @@
+class Solution {
+
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+        for(int i = n;i >= 1;i--) {
+            for(int j = i;j <= n;j++) {
+                int maxi = -1e8;
+                for(int k = i;k <= j;k++) {
+                    int cost = (nums[i-1] * nums[k] * nums[j+1]) +
+                                dp[i][k-1] + dp[k+1][j];
+                    maxi = max(maxi, cost);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+
+        return dp[1][n];
+        // return solve(nums, 1, n, dp);
+    }
+};
