f87ee94ffd53de637c034ae5345246dfac144354
Rohit <139196975+rohit-b04@users.noreply.github.com>
2025-12-08 10:17:08 +0530

    Time: 7 ms (62.17%), Space: 7.7 MB (84.51%) - LeetArchive

---
+++ LeetCode/2037-count-square-sum-triples/2037-count-square-sum-triples.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int count = 0;
+      
+        for (int a = 1; a < n; ++a) {
+            for (int b = 1; b < n; ++b) {
+                int sumOfSquares = a * a + b * b;
+              
+                int c = static_cast<int>(sqrt(sumOfSquares));
+              
+                if (c <= n && c * c == sumOfSquares) {
+                    ++count;
+                }
+            }
+        }
+      
+        return count;
+    }
+};
